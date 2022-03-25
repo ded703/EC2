87a11a43ab31f5a1371eecb18c9188be695b99e8
Eric Duquesnoy <16538036+ded703@users.noreply.github.com>
2022-03-25 22:51:51 +0100

    created main.tf with ec2 instance

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 1.1.3"
+  }
+
+
+provider "aws" {
+  region = "eu-west-1"
+  profile = "apprentices"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0069d66985b09d219"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-021b967ea4fcad010"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
